0743784c885159b183f58148ddb888db1d25aac4
Mohammed <mohammed@easyecom.io>
2024-06-18 19:56:26 +0530

    folder setup

---
+++ src/components/MobileList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./mobileList.css";
+
+export default function MobileList() {
+  return (
+    <>
+      <div className="wrapper">
+        <img
+          src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/k/g/j/t3x-5g-v2338-vivo-original-imahyyzaqhgwzfup.jpeg?q=70"
+          alt="vivo T3x 5G (Celestial Green, 128 GB)"
+        />
+        <div>
+          <p>vivo T3x 5G (Celestial Green, 128 GB)</p>
+          <p>₹ 13,499</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+    </>
+  );
+}
